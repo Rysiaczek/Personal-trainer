058b623663610e89306122bfb6aea8e2b72ff826
Ryszard Gruszczyński <90858674+Rysiaczek@users.noreply.github.com>
2024-09-09 20:11:47 +0200

    final

--- src/components/offert.js
+++ src/components/offert.js
@@ -4,17 +4,16 @@ import "../css/offert.css";
 function Offert() {
   var settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    swipeToSlide: true,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -23,7 +22,6 @@ function Offert() {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
           initialSlide: 1,
         },
       },
@@ -31,7 +29,6 @@ function Offert() {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
         },
       },
     ],
